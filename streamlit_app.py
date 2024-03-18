64403152a0fbf4d320bd498efa3bc0e848944b43
valera2005cho <163824906+valera2005cho@users.noreply.github.com>
2024-03-18 13:32:01 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,17 @@
 import streamlit as st
 
-st.title("Hello")
+with st.sidebar:
+  openai_api_key = st.text_input("OpenAI API Key: ", key="chatbot_api_key", type="password")
+
+st.title("Chatbot Valera")
+if "messages" not in st.session_state:
+  st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+  st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+  if not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.")
+    st.stop()
+
