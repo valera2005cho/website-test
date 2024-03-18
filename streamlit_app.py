700c4d6b327aa5e55e4cc4315c32550e41f2aacc
valera2005cho <163824906+valera2005cho@users.noreply.github.com>
2024-03-18 12:57:14 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
+import openai
 import streamlit as st
-from streamlit_gallery import apps, components
-from streamlit_gallery.utils.page import page_group
 
-def main():
-  page = page_group("p")
+with st.sidebar:
+  openai_api_key = st.text_input("OpenAI API Key: ", key="chatbot_api_key", type="password")
+
+st.title("Chatbot Valera")
+if "messages" not in st.session_state:
+  st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+  st.chat_message(msg["role"]).write(msg["content"])
+
+if promt := st.chat_input():
+  if not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.")
+    st.stop()
+
+  openai.api_key = openai_api_key
+  st.session_state.messages.append({"role": "user", "content": prompt})
+  st.chat_message("user").write(prompt)
+  response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+  msg = response.choices[0].message
+  st.session_state.messages.append(msg)
+  st.chat_message("assistant").write(msg.content)
