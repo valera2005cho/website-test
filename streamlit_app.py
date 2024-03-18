f13ca8a198d91bb3c42fb87119ef2059fbc9644a
valera2005cho <163824906+valera2005cho@users.noreply.github.com>
2024-03-18 13:01:03 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ if "messages" not in st.session_state:
 for msg in st.session_state.messages:
   st.chat_message(msg["role"]).write(msg["content"])
 
-if promt := st.chat_input():
+if prompt := st.chat_input():
   if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.")
     st.stop()
